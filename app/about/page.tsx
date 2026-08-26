016e1b9e4fadf7dbcdfe1fe56498f8bfded3e38c
Mehdi <mehdi.shakibapour@gmail.com>
2026-08-26 17:56:26 -0400

    fix: about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,13 +7,13 @@ const BOARD_MEMBERS = [
   { name: "Gavin Xue", role: "Founder and President", school: "GS '28", linkedin: "https://www.linkedin.com/in/gx2173", image: "/board/president.jpg" },
   { name: "Bayron Aguilar", role: "Founder and Vice President", school: "GS '29", linkedin: "https://linkedin.com/in/Bayron-Aguilar", image: "/board/vice-president1.jpeg" },
   { name: "Eren Yesiltepe", role: "Founder and Vice President", school: "GS '28", linkedin: "https://www.linkedin.com/in/erenyesiltepe/", image: "/board/vice-president2.jpeg" },
-  { name: "Joshua Becher", role: "Head of Development", school: "GS '30", linkedin: "https://www.linkedin.com/in/joshua-becher/", image: "/board/head-of-development1.jpeg"},
   { name: "Mehdi Shakibapour", role: "Head of Technology", school: "GS '27", linkedin: "https://linkedin.com/in/mehdisha", image: "/board/head-of-technology.png"},
   { name: "Noah Kim", role: "Head of Marketing", school: "GS '27", linkedin: "https://www.linkedin.com/in/noah-kim-4075772b0", image: "/board/head-of-marketing.jpeg" },
   { name: "Fatine Mohattane", role: "Head of Events", school: "GS '27", linkedin: "https://www.linkedin.com/in/fatinemohattane/", image: "/board/head-of-events.jpeg" },
   { name: "Tom Rosenzweig", role: "Head of Finance", school: "GS '28", linkedin: "https://www.linkedin.com/in/tom-rosenzweig-073219339/", image: "/board/head-of-finance.jpeg" },
   { name: "Gal Winter", role: "Co-head of Education", school: "GS '28", linkedin: "https://www.linkedin.com/in/galwinter/", image: "/board/head-of-education2.jpeg" },
   { name: "Julia Zhang", role: "Co-head of Education", school: "CC '28", linkedin: "https://www.linkedin.com/in/julia-zhang-2298a4354", image: "/board/head-of-education1.jpeg" },
+  { name: "Joshua Becher", role: "Head of Development", school: "GS '30", linkedin: "https://www.linkedin.com/in/joshua-becher/", image: "/board/head-of-development1.jpeg"},
   { name: "William Hamilton", role: "Co-head of Communication", school: "GS '30", linkedin: "https://www.linkedin.com/in/williamwesthamilton/", image: "/board/head-of-communications2.jpeg" },
 ];
 
